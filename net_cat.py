32bf984baba5ddb3049fb767d3d7975b3a656c13
Vaishnavi kalkute <vaishnavi.kalkute@punctuations.in>
2024-06-03 23:21:50 +0530

    initial sturct

---
+++ net_cat.py
@@ -0,0 +1,30 @@
+import argparse
+import socket
+import sys
+import threading
+
+def start_server():
+    pass
+
+def client_server():
+    pass
+
+def send():
+    pass
+
+def recv():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+# class NetCat:
+#     def __init__(self,host=127.0.0.1,port=5000):
+#         self.host=host
+#         self.port=port
